90bdafc9199f817d466491cb34c1891bd9a92df7
Erinn Looney-Triggs <erinn.looneytriggs@gmail.com>
2018-08-06 14:09:15 -0600

    Re-add the formats and the format types.

--- comodo_rest_api/__init__.py
+++ comodo_rest_api/__init__.py
@@ -16,6 +16,52 @@ class ComodoCA(object):
     Top level class for the Comodo CA. Only very generic 'things' go here.
     """
 
+    formats = {'AOL': 1,
+               'Apache/ModSSL': 2,
+               'Apache-SSL': 3,
+               'C2Net Stronghold': 4,
+               'Cisco 3000 Series VPN Concentrator': 33,
+               'Citrix': 34,
+               'Cobalt Raq': 5,
+               'Covalent Server Software': 6,
+               'IBM HTTP Server': 7,
+               'IBM Internet Connection Server': 8,
+               'iPlanet': 9,
+               'Java Web Server (Javasoft / Sun)': 10,
+               'Lotus Domino': 11,
+               'Lotus Domino Go!': 12,
+               'Microsoft IIS 1.x to 4.x': 13,
+               'Microsoft IIS 5.x and later': 14,
+               'Netscape Enterprise Server': 15,
+               'Netscape FastTrac': 16,
+               'Novell Web Server': 17,
+               'Oracle': 18,
+               'Quid Pro Quo': 19,
+               'R3 SSL Server': 20,
+               'Raven SSL': 21,
+               'RedHat Linux': 22,
+               'SAP Web Application Server': 23,
+               'Tomcat': 24,
+               'Website Professional': 25,
+               'WebStar 4.x and later': 26,
+               'WebTen (from Tenon)': 27,
+               'Zeus Web Server': 28,
+               'Ensim': 29,
+               'Plesk': 30,
+               'WHM/cPanel': 31,
+               'H-Sphere': 32,
+               'OTHER': -1,
+               }
+
+    format_type = [
+        'x509',     # X509, Base64 encoded
+        'x509CO',   # X509 Certificate only, Base64 encoded
+        'x509IO',   # X509 Intermediates/root only, Base64 encoded
+        'base64',   # PKCS#7 Base64 encoded
+        'bin',      # PKCS#7 Bin encoded
+        'x509IOR',  # X509 Intermediates/root only Reverse, Base64 encoded
+    ]
+
 
 class ComodoTLSService(ComodoCA):
     """
